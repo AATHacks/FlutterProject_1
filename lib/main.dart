2894a109fe18ff623f1f9ca79a4e559944ec09f3
abhijeet <abhijeet.tank@gmail.com>
2022-11-11 22:42:54 +0530

    project_2

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,3 @@
-/* import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(    
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-   
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
-    );
-  }
-}
- */
 import 'package:flutter/material.dart';
 
 void main() {
@@ -79,6 +13,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var Questions = [
+      "Why you left this IT Field behind?",
+      "Why You Stick in this government preparation?"
+    ];
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
@@ -86,7 +24,7 @@ class MyApp extends StatelessWidget {
             ),
             body: Column(
               children: [
-                Text("Why you left this IT Field behind?"),
+                Text(Questions[0]),
                 ElevatedButton(
                     onPressed: onPressedResult,
                     child: Text('Because of Dinesh')),
