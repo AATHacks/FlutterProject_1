c0f949f690ca7dce778eaaeaa1e5032d916eaf21
abhijeet <abhijeet.tank@gmail.com>
2022-11-17 22:59:26 +0530

    score added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, non_constant_identifier_names, avoid_print, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
-import 'package:project_2/center_widget.dart';
+import 'package:project_2/result.dart';
 import 'package:project_2/quiz.dart';
 
 void main() {
@@ -23,29 +23,35 @@ class _MyAppState extends State<MyApp> {
     {
       'question': 'who is the president of india?',
       'answers': [
-        'narendra modi',
-        'bhupendra patel',
-        'draupadi murmu',
-        'rahul gandhi'
+        {'text': 'narendra modi', 'score': 0},
+        {'text': 'bhupendra patel', 'score': 0},
+        {'text': 'draupadi murmu', 'score': 1},
+        {'text': 'rahul gandhi', 'score': 0},
       ]
     },
     {
       'question': 'who is god of cricket?',
       'answers': [
-        'sachin tendulkar',
-        'rahul dravid',
-        'yuvraj singh',
-        'ms dhoni'
+        {'text': 'sachin tendulkar', 'score': 1},
+        {'text': 'rahul dravid', 'score': 0},
+        {'text': 'yuvrj singh', 'score': 0},
+        {'text': 'ms dhoni', 'score': 0}
       ]
     },
     {
       'question': 'What\'s your favourite color?',
-      'answers': ['blue', 'orange', 'white']
+      'answers': [
+        {'text': 'blue', 'score': 1},
+        {'text': 'orange', 'score': 0},
+        {'text': 'white', 'score': 1},
+      ]
     }
   ];
   var _QuestionIndex = 0;
+  int _totalScore = 0;
 
-  void answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore = _totalScore + score;
     setState(() {
       _QuestionIndex = _QuestionIndex + 1;
     });
@@ -66,9 +72,9 @@ class _MyAppState extends State<MyApp> {
           body: _QuestionIndex < Questions.length
               ? Quiz(
                   questions: Questions,
-                  answerQuestion: answerQuestion,
+                  answerQuestion: _answerQuestion,
                   questionIndex: _QuestionIndex)
-              : CenterWidget(quizCompleteMessage)),
+              : CenterWidget(_totalScore)),
     );
   }
 }
