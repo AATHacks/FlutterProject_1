73b0e5b62b4521747e3056f9bb2d723919430b41
abhijeet <abhijeet.tank@gmail.com>
2022-11-19 16:44:29 +0530

    final submit

--- lib/result.dart
+++ lib/result.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class CenterWidget extends StatelessWidget {
-  CenterWidget(this.score);
+  CenterWidget(this.score, this.resetHandler);
   int score;
+  final VoidCallback resetHandler;
   String completeMessage = 'you did it!';
   String get resultPhase {
     if (score == 0) {
@@ -20,11 +21,16 @@ class CenterWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhase,
-        style: const TextStyle(
-            fontSize: 30, fontWeight: FontWeight.w600, color: Colors.green),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhase,
+            style: const TextStyle(
+                fontSize: 30, fontWeight: FontWeight.w600, color: Colors.green),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(onPressed: resetHandler, child: Text("Restart Quiz!")),
+        ],
       ),
     );
   }
